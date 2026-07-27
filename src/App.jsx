565408deb0b608c0647241e90ae1982ca733d644
khanhduy130910-eng <khanhduy130910@gmail.com>
2026-07-28 01:52:56 +0700

    Mô tả thay đổi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import { AuthProvider } from "./providers/AuthProvider";
 import AppRouter from "./routes/AppRouter";
 
 export default function App() {
-  return <AppRouter />;
+    return (
+        <AuthProvider>
+            <AppRouter />
+        </AuthProvider>
+    );
 }
